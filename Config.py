8f8b3a5cd745e9b1dbd4489d2ef1dcd7d473b834
mohammad baqer <124671029+mestern@users.noreply.github.com>
2024-10-19 14:32:57 +0330

    comments update

--- Config.py
+++ Config.py
@@ -1,9 +1,12 @@
 # username and password of admin/maker
-admin_username = "shater3"
-admin_password = "123321"
+admin_username = "username"
+admin_password = "password"
 
 # telegram channel id
-ChannelId = '-1001214282289'
+ChannelId = 'your channelID'
 
-admins = {"16085": "alirezaa", "8964": "mahdi1"}
+# app admins
+admins = {"admin": "1234"}
+
+# price of products
 price = {"m1u1": "110", "m1u2": "160", "m3u1": "240", "m3u2": "380"}
